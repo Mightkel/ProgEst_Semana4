2443d714a1d2598982f73a5ab6ae9175d7c49569
Maykel Cruz <maykelcruz@uamv.edu.ni>
2026-09-07 13:38:26 -0600

    Agregue el proyecto calculadora

---
+++ calculadora/principal.py
@@ -0,0 +1,6 @@
+import aritmetica as arit
+
+print(f"la suma es {arit.add(2, 2)}")
+print(f"la resta es {arit.sub(2, 2)}")
+print(f"la multiplicacion es {arit.mult(2, 2)}")
+print(f"la division es {arit.div(2, 2)}")
