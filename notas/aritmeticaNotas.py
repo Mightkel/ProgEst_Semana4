d0b8051e035c48d435375d8ab44c93d9af2d0061
Maykel Cruz <maykelcruz@uamv.edu.ni>
2026-09-09 10:51:45 -0600

    fix

--- notas/aritmeticaNotas.py
+++ notas/aritmeticaNotas.py
@@ -1,5 +1,6 @@
 #Funciones para leer la nota e identificar su categoria
 notes = []
+categories = []
 def addNote(note):
     notes.append(note)
     return 0
@@ -18,12 +19,16 @@ def readNotes():
                 addNote(note)
                 if note > 0 and note < 70:
                     print("Aprendizaje Inicial")
+                    categories.append("Aprendizaje Inicial")
                 elif note > 69 and note < 80:
                     print("Aprendizaje Fundamental")
+                    categories.append("Aprendizaje Fundamental")
                 elif note > 79 and note < 90:
                     print("Aprendizaje Sastifactorio")
+                    categories.append("Aprendizaje Sastifactorio")
                 elif note > 89 and note < 101:
                     print("Aprendizaje Avanzado")
+                    categories.append("Aprendizaje Avanzado")
             else:
                 print("La nota no puede ser negativa o superior a 100")
             answer = input("Desea ingresar otra nota? ( S - N ): ")
